ae1bee3399900065b84164520e6938d18ba1d5a8
Darren Tebo <darrentebo83@outlook.com>
2020-04-03 01:08:38 -0400

    new1

--- web/src/components/authorization/auth.js
+++ web/src/components/authorization/auth.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import axios from 'axios'
-import { navigate, routes } from '@redwoodjs/router'
 
 const AuthEndpoint = 'https://accounts.spotify.com/api/token'
 const clientID = 'bf79bedc80564646a92e2094fa449767'
-const redirect_uri = 'https://5e86b41906b9490007374371--peaceful-hawking-f47563.netlify.com/'
+const redirect_uri = 'http://localhost:8910'
 const client_secret = '0ee8ec5b6fd14a1aa43e79eea998731f'
+const client_token = btoa(`${clientID}${client_secret}`).toString()
 
 const Auth = () => {
     function authenticate(){
         const auth_url = `${AuthEndpoint} + ?client_id=${clientID}&response_type=token&redirect_uri=${redirect_uri}`
+console.log(client_token)
+        axios.post(
+            'https://accounts.spotify.com/api/token',
+                new URLSearchParams({
+                    grant_type: "client_credentials",
+                    client_id: `${client_token}`
+                }).toString(),
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Authorization': `Basic ${client_token}`
+                    },
+                }
+            ).then(function (response) {
+                console.log(response.data);
+            });
 
-        axios.post(auth_url, { grant_type: 'client_credentials'}, {headers: { Authorization: `Basic ${clientID}:${client_secret}`}}).then((res) => {
-            console.log(res)
+        // axios.post(AuthEndpoint, { grant_type: 'client_credentials' }, {headers: { 'Authorization': `Basic ${clientID}:${client_secret}`, "Content-Type": "application/jsodfvbdfgn"}}).then((res) => {
+        //     console.log(res)
 
-            // window.location = auth_url
-        })
+        //     // window.location = auth_url
+        // })
     }
 
     return (
