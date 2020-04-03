47625425a56543e15b03e501b89fbfcc2420c75e
Darren Tebo <darrentebo83@outlook.com>
2020-04-02 23:06:14 -0400

    update

--- LambdaTunes/web/src/components/audioplayer/audioplayer.js
+++ LambdaTunes/web/src/components/audioplayer/audioplayer.js
@@ -4,7 +4,7 @@ import session from 'src/media/sess3_6.mp3'
 const AudioPlayer = () => {
     return (
         <div>
-            <audio class="audio-player" controls src={session}></audio>
+            <audio className="audio-player" controls src={session}></audio>
         </div>
     )
 }
--- LambdaTunes/web/src/pages/HomePage/HomePage.js
+++ LambdaTunes/web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import AudioPlayer from 'src/components/audioplayer/audioplayer'
+import Auth from 'src/components/authorization/auth'
 
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
+      <Auth />
       <AudioPlayer />
     </div>
   )
