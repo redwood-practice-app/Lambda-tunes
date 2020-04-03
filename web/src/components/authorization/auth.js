63b24695a4d2871eea9f79c107ef34b37253684b
Darren Tebo <darrentebo83@outlook.com>
2020-04-02 23:47:08 -0400

    newer test

--- web/src/components/authorization/auth.js
+++ web/src/components/authorization/auth.js
@@ -2,15 +2,16 @@ import React from 'react'
 import axios from 'axios'
 import { navigate, routes } from '@redwoodjs/router'
 
-const AuthEndpoint = 'https://accounts.spotify.com/authorize'
+const AuthEndpoint = 'https://accounts.spotify.com/api/token'
 const clientID = 'bf79bedc80564646a92e2094fa449767'
 const redirect_uri = 'http://localhost:8910/'
+const client_secret = '0ee8ec5b6fd14a1aa43e79eea998731f'
 
 const Auth = () => {
     function authenticate(){
         const auth_url = `${AuthEndpoint} + ?client_id=${clientID}&response_type=token&redirect_uri=${redirect_uri}`
-        
-        axios.get(auth_url).then((res) => {
+
+        axios.post(auth_url, { grant_type: 'client_credentials'}, {headers: { Authorization: `Basic ${clientID}:${client_secret}`}}).then((res) => {
             console.log(res)
 
             // window.location = auth_url
