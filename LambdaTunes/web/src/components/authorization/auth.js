e3710a434e558db5c15aa4b6a7905b3463887f2b
Darren Tebo <darrentebo83@outlook.com>
2020-04-02 22:10:50 -0400

    initial commit

---
+++ LambdaTunes/web/src/components/authorization/auth.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthEndpoint = 'https://accounts.spotify.com/authorize'
+const clientID = 'bf79bedc80564646a92e2094fa449767'
+const redirectURL = 'http://localhost:8910/'
+
+const Auth = () => {
+    
+}
---
+++ LambdaTunes/web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import AudioPlayer from 'src/components/audioplayer/audioplayer'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <AudioPlayer />
+    </div>
+  )
+}
+
+export default HomePage
