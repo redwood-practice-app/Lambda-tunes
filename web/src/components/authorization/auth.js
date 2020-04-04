ece6bc932f7976894b98f7b37255ac1e611f1d49
Darren Tebo <darrentebo83@outlook.com>
2020-04-03 21:16:38 -0400

    testing

--- web/src/components/authorization/auth.js
+++ web/src/components/authorization/auth.js
@@ -5,17 +5,18 @@ const AuthEndpoint = 'https://accounts.spotify.com/api/token'
 const clientID = 'bf79bedc80564646a92e2094fa449767'
 const redirect_uri = 'https://peaceful-hawking-f47563.netlify.com/'
 const client_secret = '0ee8ec5b6fd14a1aa43e79eea998731f'
-const client_token = btoa(`${clientID}${client_secret}`).toString()
+const client_token = btoa(`${clientID}${client_secret}`)
 
 const Auth = () => {
     function authenticate(){
-        const auth_url = `${AuthEndpoint} + ?client_id=${clientID}&response_type=token&redirect_uri=${redirect_uri}`
+        const auth_url = `${AuthEndpoint} + ?client_id=${client_token}&response_type=token&redirect_uri=${redirect_uri}`
 console.log(client_token)
         axios.post(
-            'https://accounts.spotify.com/api/token',
+            encodeURIComponent(`${AuthEndpoint}`),
                 new URLSearchParams({
                     grant_type: "client_credentials",
-                    client_id: `${client_token}`
+                    code: `${client_token}`,
+                    redirect_uri: `${redirect_uri}`
                 }).toString(),
                 {
                     headers: {
