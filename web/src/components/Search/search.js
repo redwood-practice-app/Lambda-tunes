8874e78be0bc5ccf196a3e7a07e7a56bc454298d
Darren Tebo <darrentebo83@outlook.com>
2020-04-04 19:07:18 -0400

    adding missing files

---
+++ web/src/components/Search/search.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import axios from 'axios'
+
+const Search = () => {
+  function apiRequest(options = {}, callback){
+    //wrapper for all axios calls
+    axios(options)
+      .then(callback(resp))
+  }
+
+  function axiosCall() {
+    let hash = window.location.hash
+      .substring(1)
+      .split('&')
+      .reduce(function (initial, item) {
+        if (item) {
+          var parts = item.split('=')
+          initial[parts[0]] = decodeURIComponent(parts[1])
+        }
+        return initial
+      }, {})
+
+    apiRequest({
+      method: 'get',
+      url: 'https://api.spotify.com/v1/me/top/artists',
+      headers: { Authorization: 'Bearer ' + hash.access_token }
+    }, (resp) => {
+      console.log(resp)
+    })
+
+    // axios
+    //   .get('https://api.spotify.com/v1/me/top/artists', {
+    //     headers: { Authorization: 'Bearer ' + hash.access_token },
+    //   })
+    //   .then((response) => {
+    //     console.log(response)
+    //   })
+  }
+
+  return (
+    <div>
+      <button onClick={axiosCall}>Find top artists!</button>
+    </div>
+  )
+}
+
+export default Search
