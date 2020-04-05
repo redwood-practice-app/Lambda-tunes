f19cdbd63d7905b14ee508d0e5945ca1a3cda182
Darren Tebo <darrentebo83@outlook.com>
2020-04-05 00:55:20 -0400

    added fontawesome packages

--- web/src/components/audioplayer/audioplayer.js
+++ web/src/components/audioplayer/audioplayer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const AudioPlayer = () => {
     return (
         <div>
             <section className="custom-player flexbox horizontal">
-                <div className="btn-play"></div>
+                <div className="btn-play"><FontAwesomeIcon icon={faCoffee} /></div>
                 <audio className="audio-player" controls></audio>
             </section>
         </div>
