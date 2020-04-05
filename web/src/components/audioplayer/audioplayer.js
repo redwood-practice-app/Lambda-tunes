ddc29b42a9505b911c105aea319be9c0498c6970
Darren Tebo <darrentebo83@outlook.com>
2020-04-05 01:09:55 -0400

    added play button

--- web/src/components/audioplayer/audioplayer.js
+++ web/src/components/audioplayer/audioplayer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
 
 const AudioPlayer = () => {
     return (
         <div>
             <section className="custom-player flexbox horizontal">
-                <div className="btn-play"><FontAwesomeIcon icon={faCoffee} /></div>
+                <div className="btn-play flexbox horizontal"><FontAwesomeIcon icon={faPlayCircle} /></div>
                 <audio className="audio-player" controls></audio>
             </section>
         </div>
